0644eaacef119b03e62ab0805c0fd7656ddc8aec
NextOgurchik <mihanfedotov@yandex.ru>
2020-09-13 23:22:13 +0300

    Changed layout

--- dbo.Honeys.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Honeys] (
-    [Id]       INT            IDENTITY (1, 1) NULL,
-    [Name]     NVARCHAR (MAX) NULL,
-    [Producer] NVARCHAR (MAX) NULL,
-    [Price]    INT            NOT NULL,
-    CONSTRAINT [PK_dbo.Honeys] PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
